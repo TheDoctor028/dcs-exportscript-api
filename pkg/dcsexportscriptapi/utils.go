cd6ab43d10d9c5f478312f1d32754d655c084d59
kristofhetenyi <kristofhetenyi228@gmail.com>
2022-06-26 18:57:46 +0200

    Extract UID with value from the data stream

--- pkg/dcsexportscriptapi/utils.go
+++ pkg/dcsexportscriptapi/utils.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"encoding/ascii85"
 	"encoding/gob"
-	"expvar"
+	"regexp"
 	"strings"
 )
 
@@ -30,25 +30,60 @@ func DeserializeDataStream(dataStream *[]byte) (string, error) {
 	return b.String(), nil
 }
 
-func extractUIAndValue(str string) []expvar.KeyValue {
-	if len(str) == 0 {
+func testStrLen(str string) bool {
+	return len(str) == 0
+}
+
+func ExtractUIDAndValue(str string, splitChar string) map[string]string {
+	if testStrLen(str) {
 		return nil
 	}
-
-	//var kv []expvar.KeyValue
 
 	// Remove memory address from the start of the string
 	strWOAddr := strings.Split(str, "*")[1]
 
-	if len(strWOAddr) == 0 {
+	if testStrLen(strWOAddr) {
 		return nil
 	}
 
+	// Remove EOL(s) and spaces from the string
 	strSingleLine := strings.ReplaceAll(strWOAddr, "\n", "")
+	strSingleLine = strings.ReplaceAll(strSingleLine, " ", "")
 
-	if len(strSingleLine) == 0 {
+	if testStrLen(strSingleLine) {
 		return nil
 	}
 
-	return nil
+	// Split the string to sub strings by split char
+	strSplitedByChar := strings.Split(strSingleLine, splitChar)
+
+	if len(strSplitedByChar) == 0 {
+		return nil
+	}
+
+	keyValStrMap, done := convertKeyValStrToMap(strSplitedByChar)
+	if !done {
+		return nil
+	}
+
+	return keyValStrMap
+}
+
+func convertKeyValStrToMap(strSplitedByChar []string) (map[string]string, bool) {
+	keyValueMap := make(map[string]string)
+
+	// Remove values without UID(key)
+	for i := range strSplitedByChar {
+		iStr := strSplitedByChar[i]
+		matched, err := regexp.MatchString("^\\d+=[-]?\\d+[.]?\\d*$", iStr)
+		if err != nil {
+			return nil, false
+		}
+
+		if matched {
+			keyValSplited := strings.Split(iStr, "=")
+			keyValueMap[keyValSplited[0]] = keyValSplited[1]
+		}
+	}
+	return keyValueMap, true
 }
--- pkg/dcsexportscriptapi/utils_test.go
+++ pkg/dcsexportscriptapi/utils_test.go
@@ -1,6 +1,7 @@
 package dcsexportscriptapi
 
 import (
+	"reflect"
 	"regexp"
 	"testing"
 )
@@ -22,4 +23,30 @@ func TestDeserializeDataStream(t *testing.T) {
 	if !expected.MatchString(res) && err == nil {
 		t.Fatalf("Expected string %s from byte array %d, got %s. Error: %s", expectedString, buff, res, err)
 	}
+}
+
+func TestExtractUIDAndValue(t *testing.T) {
+	text := "62b74d61*146=0.0017:141=0.0074 \n:128=-0.0682:146=0.0008:180=0.4000:141=0.0018\n=0:184=0:200=0"
+
+	expected := getExtractUIDAndValueExpectedValues()
+
+	t.Logf("Testing with %s", text)
+
+	res := ExtractUIDAndValue(text, ":")
+
+	if !reflect.DeepEqual(res, expected) {
+		t.Fatalf("Expected: %s \n Got: %s", expected, res)
+	}
+}
+
+func getExtractUIDAndValueExpectedValues() map[string]string {
+	keyValueMap := make(map[string]string)
+	keyValueMap["200"] = "0"
+	keyValueMap["184"] = "0"
+	keyValueMap["141"] = "0.0074"
+	keyValueMap["180"] = "0.4000"
+	keyValueMap["146"] = "0.0008"
+	keyValueMap["128"] = "-0.0682"
+
+	return keyValueMap
 }
