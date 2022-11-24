39436752b9c0544d17dbdf685584c074ae0da2f9
kristofhetenyi <kristofhetenyi228@gmail.com>
2022-11-24 21:01:50 +0100

    Add simple ws

--- client/dcs-exportscript-client/src/services/dcs.service.ts
+++ client/dcs-exportscript-client/src/services/dcs.service.ts
@@ -1,18 +1,56 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Observer, of, Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { AnonymousSubject } from 'rxjs/internal-compatibility';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DcsService {
-  constructor(private readonly httpClient: HttpClient) { }
+  // The default address of the DCS GO server. (with port but without protocol)
+  public address = 'localhost:3333';
+
+  protected exportAPI$: Subject<string>;
+
+  constructor(
+    protected readonly httpclient: HttpClient
+  ) { }
 
   /**
    * Queries the hello end point.
-   * If it will return a 'hello' string.
+   * If it returns a 'hello' string.
    */
   public hello(): Observable<string> {
-    return this.httpClient.get();
+    return this.httpclient.get<string>('http://' + this.address + '/hello');
   }
+
+  private connectWebsocket(): Subject<string> {
+    if(!this.exportAPI$) {
+      this.exportAPI$ = this.createWebsocket();
+    }
+    return this.exportAPI$;
+  }
+
+  private createWebsocket(): AnonymousSubject<string> {
+    const ws = new WebSocket('ws://' + this.address + '/raw');
+    const observable = new Observable((obs: Observer<string>) => {
+      ws.onmessage = obs.next.bind(obs);
+      ws.onerror = obs.error.bind(obs);
+      ws.onclose = obs.complete.bind(obs);
+      return ws.close.bind(ws);
+    });
+    const observer = {
+      error: null,
+      complete: null,
+      next: (data: any) => {
+        console.log('Message sent to websocket: ', data);
+        if (ws.readyState === WebSocket.OPEN) {
+          ws.send(JSON.stringify(data));
+        }
+      }
+    };
+    return new AnonymousSubject<string>(observer, observable);
+  };
+
+
 }
